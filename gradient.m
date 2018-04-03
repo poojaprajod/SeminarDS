c64295a613b66babb370004a8093025eae478544
Pooja Prajod <poojaprajod@gmail.com>
2018-04-04 01:21:15 +0200

    task1
    Signed-off-by: Pooja Prajod <poojaprajod@gmail.com>

--- gradient.m
+++ gradient.m
@@ -5,12 +5,12 @@ gdim = h*(w-1)+w*(h-1);
 
 % collect triplets here
 i = [(1:gdim)'; (1:gdim)'];
-a = [1:w*h]; a(w:w:w*h)=[]; 
-b = [w+1:w*h];
-c = [1:w*h]; c(1:w:w*h)=[];
-d = [1:w*(h-1)];
+a = [1:w*h]; a(h:h:w*h)=[]; 
+b = [h+1:w*h];
+c = [1:w*h]; c(1:h:w*h)=[];
+d = [1:h*(w-1)];
 j = [a';b';c';d'];
-v = [-(1/2)*ones(gdim,1); (1/2)*ones(gdim,1)];
+v = [(1/2)*ones(gdim,1); -(1/2)*ones(gdim,1)];
 
 G = sparse(i,j,v);
 
